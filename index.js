28afd5ce26ad64069016dc870b74d0833134b272
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-09-04 11:26:51 -0400

    commit tree 407c63815f2eaa09f00ec859d2421236040188ce

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+
+
+
+
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers() {
+    return drivers.slice(2,4)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(x) {
+    return function(y) {
+        return x*y;
+    }
+    //Not sure why I cant put the 3*12 it sais in the test?
+}
+function fareDoubler() {
+    return (2*10)
+}
+function fareTripler() {
+    return (3*12)
+}
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers); 
+  }
